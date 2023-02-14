fd191034ea4da7405b80bb3bab1f95f649c241eb
Tsimafei Valshonak <tima.valshonok@gmail.com>
2023-02-14 16:21:36 +0400

    Change ports and start script

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 fetchWeather = function (location) {
-	const url = "http://localhost:3000/weather?address=" + location;
+	const url = "/weather?address=" + location;
 
 	fetch(url).then((response) => {
 		response.json().then((data) => {
